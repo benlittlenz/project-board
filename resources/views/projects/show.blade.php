96a7824ebe7beb9367c276835f20575f5eedbaae
nzbenji <benlittle53@gmail.com>
2019-12-30 13:40:01 +1300

    added base styling to projects page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,40 @@
 @extends ('layouts.app')
 
 @section ('content')
-    <h1>{{$project->title}}</h1>
-    <div>{{$project->description}}</div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-gray-500 text-sm font-normal">
+               <a href="/projects" class="text-gray-500 text-sm font-normal no-underline">My Projects / {{$project->title}}</a> 
+            </p>
+
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="flex">
+            <div class="w-3/4 px-3 mb-8">
+                <div class="mb-6">
+                    <h2 class="text-lg text-gray-500 font-normal mb-3">Tasks</h2>
+                    <div class="card mb-3">Lorem Ipsum</div>
+                    <div class="card mb-3">Lorem Ipsum</div>
+                    <div class="card mb-3">Lorem Ipsum</div>
+                    <div class="card">Lorem Ipsum</div>
+                </div>
+ 
+                
+                <div>
+                    <h2 class="text-lg text-gray-500 font-normal mb-3">Notes</h2>
+                    <textarea class="card w-full" style="min-height: 200px">Lorem Ipsum</textarea>
+                </div>
+
+            </div>
+            <div class="w-1/4 px-3">
+                @include('projects.card')
+                
+            </div>
+        </div>
+    </main>
+
 
 @endsection
